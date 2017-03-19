fb2145354d6f048513fe2fae0ef50a67d5d729b7
xiangguo <1109284509@qq.com>
2017-03-19 16:03:45 +0800

    base code complete.

--- src/TianGongMonitor.scala
+++ src/TianGongMonitor.scala
@@ -2,22 +2,26 @@
   * Created by Xiangguo.Kong on 2017/3/16.
   */
 
-import java.net.{ServerSocket,Socket}
-import java.io.{InputStream, BufferedInputStream, PrintStream,InputStreamReader,BufferedReader }
-import collection.mutable.{ArrayBuffer, SynchronizedBuffer, HashMap, SynchronizedMap}
+import java.net.{ServerSocket, Socket}
+import java.io.{BufferedInputStream, BufferedReader, File, InputStream, InputStreamReader, PrintStream, PrintWriter}
+
+import collection.mutable.{ArrayBuffer, HashMap, SynchronizedBuffer, SynchronizedMap}
+import scala.collection.mutable
+import scala.io.Source
 
 object TianGongMonitor {
 
   case class User(sock:Socket,is:BufferedReader,ps:PrintStream,name:String)
-  val message_boxs = new HashMap[String,String] with SynchronizedMap[String,String]
+  var message_boxs = new HashMap[String,String] with SynchronizedMap[String,String]
+  val road_map = new ArrayBuffer[String] with SynchronizedBuffer[String]
 
   def main(args : Array[String]) : Unit = {
 
     val users = new ArrayBuffer[User] with SynchronizedBuffer[User]
-    val road_map = new ArrayBuffer[String] with SynchronizedBuffer[String]
-    road_map += ("0,0,1;10,0,1;50,0,1","0,0,1;0,2000,1;0,5000,1", "0,0,1;1000,1000,1;5000,5000,1", "0,0,1;20,0,1;30,10,1", "0,0,1;10,0,1;20,10,1")
-//    road_map += ("0,0,1;10,0,1;50,0,1","0,0,1;0,2000,1;0,5000,1", "0,0,1;1000,1000,1;5000,5000,1", "0,0,1;20,0,1;30,10,1")
-//    val message_boxs = new HashMap[String,String]
+//    road_map += ("0,0,1;10,0,1;50,0,1","0,0,1;0,2000,1;0,5000,1", "0,0,1;1000,1000,1;5000,5000,1", "0,0,1;20,0,1;30,10,1", "0,0,1;10,0,1;20,10,1")
+
+    //load road map.
+    load_road_map()
 
     val ss = new ServerSocket(8088)
     actors.Actor.actor{
@@ -54,45 +58,67 @@ object TianGongMonitor {
 
           //update status for client.
           message_boxs(user.name) = recieve
-//          println(message_boxs)
-//          println(message_boxs(user.name))
 
           i = i + 1
-          if (input.startsWith("End")){
-            user.ps.println("Complete Task! please exit: " + user.name)
-            println(user.name + " have complete task and will exit.")
-          } else {
-            user.ps.println(" Hello Yu!" + user.name + " " + i)
+          //checked whether or not complete task for per YuTu
+          if (message_boxs.contains(user.name)){
+            if ( input.startsWith("End")){
+              user.ps.println("Complete Task! please exit: " + user.name)
+              println(user.name + " have completed task and will exit.")
+              message_boxs -= user.name
+            } else {
+              user.ps.println(" Hello Yu!" + user.name + " " + i)
+            }
           }
+
         }
 
+        // print monitor message.
         Thread.sleep(500)
         println("=="*80)
         for ((k,v) <- message_boxs) {
           println("--"*80)
-//          println(k)
-          println(v)
+          val ms = parse_yutu_message(v)
+
+          if (ms.nonEmpty && ms.length==2) {
+
+            println("Name: " + k + " current coordinate:["+ ms(0)(0) +','+ ms(0)(1) + "] destination coordinate:[" + ms(1)(0) +','+ ms(1)(2) +
+                      "] speed:[" + ms(0)(2) + "] angle:[" + ms(0)(3) + "]"
+                      )
+          }
 
         }
 
       }
 
-      // print monitor message.
-//      while (true) {
-//        Thread.sleep(2000)
-//        println("=="*80)
-//        for ((k,v) <- message_boxs) {
-//          println("--"*80)
-//          println(k)
-//          println(v)
-//          println(message_boxs)
-//        }
-//      }
 
     }
 
 
 
+  }
+
+  //parse message from client from YuTu
+  def parse_yutu_message(message:String):Array[List[String]] = {
+    var yms = new ArrayBuffer[List[String]]
+    val ms = message.split("=>")
+    val tms = ms.last.split(';')
+
+    for (x <- tms){
+      val xs = x.split(',')
+      yms += xs.toList
+    }
+
+    yms.toArray
+  }
+
+  //load  road map for Yutu.
+  def load_road_map():Unit = {
+    Source.fromFile("road_map.txt").getLines.foreach{
+      x => road_map += x.toString
+    }
+
+//    result.foreach(x => println(x))
   }
 
 }
--- src/YuTu.scala
+++ src/YuTu.scala
@@ -44,10 +44,6 @@ object YuTu {
             sys.exit()
           }
 
-          //          Thread.sleep(2000)
-          //          println("report man!")
-          //          os.println("Hello Server!")
-
         }
       }
     }
@@ -58,11 +54,9 @@ object YuTu {
     os.println("YuTu"+id)
     while(task_not_complete) {
       Thread.sleep(1000)
-      timer_cout = timer_cout + 1
-
+      timer_cout = timer_cout + 1 //timer for Yutu
       i = i + 1
-//      os.println("Hi" + i)
-//      println(road_map)
+
       val position = report_postion(current_road)
       os.println(position)
     }
@@ -77,15 +71,15 @@ object YuTu {
   def set_current_map(task_map:ArrayBuffer[Tuple4[Float,Float,Float, Float]]): Unit = {
     current_road(0) = task_map.head
     current_road(1) = task_map(1)
-    println("]"*30)
-    println(task_map(1)._1)
-    println(task_map(1)._2)
-//    current_road = task_map.slice(0,2)
+//    println("=="*30)
+//    println(task_map(1)._1)
+//    println(task_map(1)._2)
+
   }
 
   //calulate angle of position.
   def caluate_angle(current_road:Array[Tuple4[Float,Float,Float, Float]]): Unit = {
-//      println("ll"*80)
+
       val x:Float = current_road(1)._1 -  current_road.head._1
       val y:Float = current_road(1)._2 - current_road.head._2
       val xy2 = math.pow(x,2) + math.pow(y,2)
@@ -98,14 +92,11 @@ object YuTu {
   //calculate positon of running Yutu.
   def report_postion(current_road:Array[Tuple4[Float,Float,Float, Float]]):String = {
     //current coordinate
-    val x: Float = timer_cout * 1 * angle(1)
-    //t * v * cos
+    val x: Float = timer_cout * 1 * angle(1) //t * v * cos
     val y: Float = timer_cout * 1 * angle(0) //t * v * sin
 
     //whether or not reach to next coordinate
     //current road length
-//    val dx = x - current_road.last._1
-//    val dy = y - current_road.last._2
     val dl2 = math.pow(x, 2) + math.pow(y, 2)
     val dl = math.sqrt(dl2)
     //total road length
@@ -117,17 +108,12 @@ object YuTu {
     println("@@"*30)
     println(dl)
     println(pl)
-//    println(current_road.last._1)
-//    println(current_road.last._2)
-
-    println(px)
-    println(py)
 
     var result = x.toString + ',' + y.toString + ',' + 1 + ',' + angle(0).toString + ';' + current_road.last._1.toString + ',' + current_road.last._2.toString + ',' + current_road.last._3.toString + ',' + angle(0).toString
     if (dl > pl) {
       if (road_map.length == 2) {
         result = "End Task!"
-//        sys.exit()
+
         result
       } else {
         //set next road map
@@ -150,20 +136,14 @@ object YuTu {
   //for parse road map
   def parseRoadMap(maps:String):Unit = {
       val tmp:Array[String] = maps.split("=>")
-      println("-="*50)
-//      println(tmp.length)
-//      println(tmp(0))
-//      println(tmp(1))
-
+//      println("-="*50)
       val tmp0 = tmp(1).split(';')
-      println("["*40)
-      for (x <- tmp0) {println(x)}
+
+//      for (x <- tmp0) {println(x)}
       for (i <- tmp0){
           val tmp1 = i.split(',')
-//        println(tmp1.length)
           val tmp2 = for (x <- tmp1)  yield x.toFloat
           road_map += Tuple4(tmp2(0), tmp2(1), tmp2(2),0)
-//          println(road_map)
       }
 
   }
